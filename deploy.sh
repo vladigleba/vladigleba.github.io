51641da48f84f291c8aa19e6378eff179333f106
Vladi Gleba <hi@vladigleba.com>
2025-04-01 17:05:33 -0700

    latest

--- deploy.sh
+++ deploy.sh
@@ -7,25 +7,15 @@ PROD_BRANCH="master"
 SOURCE_BRANCH="source"
 CURRENT_TIME_UTC=$(date -u +"%Y-%m-%d %H:%M:%S UTC")
 
-echo "Building Eleventy site..."
-npx eleventy
-
-echo "Switching to $PROD_BRANCH branch..."
+cp -r _site/* ../tmp
 git checkout $PROD_BRANCH
 
-echo "Cleaning up old files..."
 rm -rf *
+mv ../tmp/* .
 
-echo "Copying new build..."
-git checkout $SOURCE_BRANCH -- _site
-mv _site/* .
-
-echo "Deploying to GitHub Pages..."
 git add .
 git commit -m "Site deployed at $CURRENT_TIME_UTC"
 git push -f origin $PROD_BRANCH
-
-echo "Switching back to $SOURCE_BRANCH..."
 git checkout $SOURCE_BRANCH
 
 echo "Deployment complete!"
