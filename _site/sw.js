a38d6b2eabdee8857e4b50c2c4d2c87d6f28d13a
Vladi Gleba <hi@vladigleba.com>
2026-01-13 21:50:00 -0800

    [c] cors test

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1768369461269';
+const CACHE_NAME = 'core-assets-1768369785188';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
