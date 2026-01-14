d808a17ab95bb1443d0f4deb2a712cd74004a63d
Vladi Gleba <hi@vladigleba.com>
2026-01-13 21:44:33 -0800

    [c] simplify

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1768368854784';
+const CACHE_NAME = 'core-assets-1768369461269';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
