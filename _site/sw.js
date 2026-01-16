caea5d10ad7d9f197697bce6a2781c593884ea2f
Vladi Gleba <hi@vladigleba.com>
2026-01-15 21:52:48 -0800

    [d] reduce search icon

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1768542052659';
+const CACHE_NAME = 'core-assets-1768542738597';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
