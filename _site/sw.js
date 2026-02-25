bb11c43f44a58aa90f89a9a101667764b1d26ccd
Vladi Gleba <hi@vladigleba.com>
2026-02-24 21:54:10 -0800

    [w] citation

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1771983673399';
+const CACHE_NAME = 'core-assets-1771998834122';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
