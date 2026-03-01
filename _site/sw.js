84277ba96ef61b722b2cfac4cc732549198597dd
Vladi Gleba <hi@vladigleba.com>
2026-02-28 20:29:23 -0800

    [d] refine featured

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1772156771676';
+const CACHE_NAME = 'core-assets-1772339330166';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
