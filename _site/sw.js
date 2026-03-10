82808736ce8f3aeed30041821f0a58c0067ae38c
Vladi Gleba <hi@vladigleba.com>
2026-03-09 21:09:35 -0700

    [w] speyer

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1772760522140';
+const CACHE_NAME = 'core-assets-1773115682368';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
