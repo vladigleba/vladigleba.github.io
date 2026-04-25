f04efab0ed5fb5f3bc583d12539f24aa731acb92
Vladi Gleba <hi@vladigleba.com>
2026-04-24 19:49:47 -0700

    [w] done

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1776991355570';
+const CACHE_NAME = 'core-assets-1777077505197';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
