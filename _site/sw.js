fb50d4721b8ddecaa086363a78f7d62d9421e8fe
Vladi Gleba <hi@vladigleba.com>
2026-06-11 20:36:11 -0700

    [c] add email signup form

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1780462736304';
+const CACHE_NAME = 'core-assets-1781223781277';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
