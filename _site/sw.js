4a0a27ee7aa6d76694f8242d33e73004b50e7c0b
Vladi Gleba <hi@vladigleba.com>
2026-06-16 21:28:43 -0700

    [d] bump stat count

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1781661127866';
+const CACHE_NAME = 'core-assets-1781670500228';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
