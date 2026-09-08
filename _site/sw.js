e9b3461ef834728a654aad9dcea83030390dab3b
Vladi Gleba <hi@vladigleba.com>
2026-09-07 21:38:48 -0700

    [w] redo

--- _site/sw.js
+++ _site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'core-assets-1788666806495';
+const CACHE_NAME = 'core-assets-1788842283168';
 const FONT_CACHE = 'google-fonts-v1';
 const IMAGE_CACHE = 'images-v1';
 const CORE_ASSETS = [
